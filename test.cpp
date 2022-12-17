f98f3ffde5b4ea132825f6c9028255957d4341b0
Luo-Yuze <80147648+Luo-Yuze@users.noreply.github.com>
2022-12-17 21:08:05 +0800

    Create test.cpp

---
+++ test.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int a = 0;
+    a = 1;
+    a = 2;
+    int b = a;
+    cout << a + b << endl;
+    printf("hello world\n");
+    system("pause");
+    return 0;
+}
